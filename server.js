3f0683f341526aadbf2e1d0f9d12cc491ecec8fe
Stevtm <steventmaddison@outlook.com>
2021-05-04 21:14:22 -0400

    set up html routes

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const htmlRoutes = require("./routes/htmlRoutes/index");
 
 // express app declaration
 const PORT = process.env.PORT || 3001;
@@ -7,10 +8,11 @@ const app = express();
 // epress middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 app.use(express.static("public"));
 
 // api and html routes
-// app.use("/", htmlRoutes);
+app.use("/", htmlRoutes);
 
 app.listen(PORT, () => {
 	console.log(`API server now on port ${PORT}!`);
