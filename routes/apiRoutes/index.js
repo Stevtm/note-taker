8a68605396bbec2fb1a343a1035e8bea00d97dcf
Stevtm <steventmaddison@outlook.com>
2021-05-08 22:09:09 -0400

    make note ids increment by 1 every time a new one is added, regardless of existing ids

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const fs = require("fs");
 const path = require("path");
-const { notes } = require("../../db/db.json");
+let { notes, id_count } = require("../../db/db.json");
 
 router.get("/notes", (req, res) => {
 	// response is array of objects from db.json
@@ -10,7 +10,7 @@ router.get("/notes", (req, res) => {
 
 router.post("/notes", (req, res) => {
 	// set id based on what the next index of the array will be (rethink this because notes can be deleted)
-	req.body.id = notes.length + 1;
+	req.body.id = id_count++;
 
 	// add new note object to the notes array
 	notes.push(req.body);
@@ -18,7 +18,7 @@ router.post("/notes", (req, res) => {
 	// update db.json with the most recent notes array
 	fs.writeFileSync(
 		path.join(__dirname, "../../db/db.json"),
-		JSON.stringify({ notes: notes })
+		JSON.stringify({ notes: notes, id_count: id_count })
 	);
 
 	res.json(req.body);
